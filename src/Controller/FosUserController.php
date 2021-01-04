669dd4ba8f75950234090984b735953073c30d6f
Ahmadou Gueye <amdou69@gmail.com>
2021-01-04 16:10:55 +0000

    https://trello.com/c/yNHLKx79

--- src/Controller/FosUserController.php
+++ src/Controller/FosUserController.php
@@ -7,6 +7,7 @@ use App\Form\FosUserType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use App\Utils\Utils;
@@ -15,33 +16,36 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 /**
  * @Route("/api/fos_user")
  */
-class FosUserController extends AbstractController {
+class FosUserController extends AbstractController
+{
 
     /**
      * @Rest\Get(path="/", name="fos_user_index")
      * @Rest\View(StatusCode = 200)
      * @IsGranted("ROLE_FOSUSER_LISTE")
      */
-    public function index(): array {
+    public function index(): array
+    {
         $fosUsers = $this->getDoctrine()
-                ->getRepository(FosUser::class)
-                ->findAll();
+            ->getRepository(FosUser::class)
+            ->findAll();
 
         return count($fosUsers) ? $fosUsers : [];
     }
-    
+
     /**
      * @Rest\Get(path="/public/check-user-existence-admission/{email}", name="fos_user_check_existence_admission")
      * @Rest\View(StatusCode = 200)
      */
-    public function checkUserExistenceForAdmission($email) {
+    public function checkUserExistenceForAdmission($email)
+    {
         $fosUser = $this->getDoctrine()
-                ->getRepository(FosUser::class)
-                ->findOneByEmail($email);
-        if(!$fosUser) {
+            ->getRepository(FosUser::class)
+            ->findOneByEmail($email);
+        if (!$fosUser) {
             throw $this->createNotFoundException("Le mail indiqué ne correspond à aucun utilisateur.");
         }
-        if(!$fosUser->getAdmission()) {
+        if (!$fosUser->getAdmission()) {
             throw $this->createNotFoundException("Cet utilisateur n'est pas habilité à acceder à la plateforme admission, merci de contacter votre administrateur.");
         }
 
@@ -49,17 +53,41 @@ class FosUserController extends AbstractController {
     }
 
     /**
+     * @Rest\Get(path="/public/validate/{email}", name="fos_user_validate_account_by_email")
+     * @Rest\View(statusCode = 200)
+     * @param Request $request
+     * @param $email
+     * @return FosUser
+     */
+    public function validateUserByEmail(Request $request, $email)
+    {
+        /** @var FosUser $user */
+        $user = $this->getDoctrine()
+            ->getRepository(FosUser::class)
+            ->findOneBy(compact('email'));
+
+        if(!$user) throw new BadRequestHttpException("Cette adresse e-mail n'est associé à aucun compte.");
+
+        if (!$user->isEnabled()) throw new BadRequestHttpException("Votre compte n'est pas encore actif");
+
+        if (!in_array($user->getProfession()->getCodeprofil(),['ADMIN', 'ETU', 'DSOS'])) throw new BadRequestHttpException("Vous n'êtes pas autorisé à vous connecter à la plateforme");
+
+        return $user;
+    }
+
+    /**
      * @Rest\Post(path="/public/reset-password/", name="user_password_reset")
      * @Rest\View(StatusCode=200)
      */
-    public function resetPassword(Request $request, \Swift_Mailer $mailer): bool {
+    public function resetPassword(Request $request, \Swift_Mailer $mailer): bool
+    {
         $email = $request->getContent();
         if (!$email) {
             throw $this->createNotFoundException("Adresse email introuvable");
         }
         $em = $this->getDoctrine()->getManager();
         $linkedUser = $em->getRepository(FosUser::class)
-                ->findOneByEmail($email);
+            ->findOneByEmail($email);
         if (!$linkedUser) {
             throw $this->createAccessDeniedException("Cette adresse email n'est asssocié à aucun utilisateur, merci de vérifier.");
         }
@@ -67,14 +95,14 @@ class FosUserController extends AbstractController {
         $linkedUser->setPasswordRequestedAt(new \DateTime());
         $em->flush();
         $message = (new \Swift_Message('Lien de réinitialisation du mot de passe.'))
-                ->setFrom(\App\Utils\Utils::$senderEmail)
-                ->setTo($linkedUser->getEmail())
-                ->setBody(
+            ->setFrom(\App\Utils\Utils::$senderEmail)
+            ->setTo($linkedUser->getEmail())
+            ->setBody(
                 $this->renderView(
-                        'emails/forgot-password/etudiant.html.twig', ['user' => $linkedUser,
-                    'link' => \App\Utils\Utils::$lienResetEtudiantPassword . $linkedUser->getConfirmationToken()]
+                    'emails/forgot-password/etudiant.html.twig', ['user' => $linkedUser,
+                        'link' => \App\Utils\Utils::$lienResetEtudiantPassword . $linkedUser->getConfirmationToken()]
                 ), 'text/html'
-        );
+            );
         $mailer->send($message);
 
         return true;
@@ -84,14 +112,15 @@ class FosUserController extends AbstractController {
      * @Rest\Post(path="/public/check-token/", name="user_token_check")
      * @Rest\View(StatusCode=200)
      */
-    public function checkToken(Request $request): FosUser {
+    public function checkToken(Request $request): FosUser
+    {
         $token = $request->getContent();
         if (!$token) {
             throw $this->createNotFoundException("Lien invalide !");
         }
         $em = $this->getDoctrine()->getManager();
         $linkedUser = $em->getRepository(FosUser::class)
-                ->findOneByConfirmationToken($token);
+            ->findOneByConfirmationToken($token);
         if (!$linkedUser) {
             throw $this->createAccessDeniedException("Ce lien n'est plus valable.");
         }
@@ -112,7 +141,8 @@ class FosUserController extends AbstractController {
      * @Rest\Post(path="/public/update-password/", name="user_update_password")
      * @Rest\View(StatusCode=200)
      */
-    public function updatePassword(Request $request, \Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface $passwordEncoder): FosUser {
+    public function updatePassword(Request $request, \Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface $passwordEncoder): FosUser
+    {
         $updateData = json_decode($request->getContent());
         if (!isset($updateData->password)) {
             throw $this->createNotFoundException("Mot de passe introuvable");
@@ -125,7 +155,7 @@ class FosUserController extends AbstractController {
 
         $em = $this->getDoctrine()->getManager();
         $linkedUser = $em->getRepository(FosUser::class)
-                ->findOneByEmail($email);
+            ->findOneByEmail($email);
         if (!$linkedUser) {
             throw $this->createAccessDeniedException("Information incorrecte...");
         }
@@ -150,7 +180,8 @@ class FosUserController extends AbstractController {
      * @Rest\View(StatusCode=200)
      * @IsGranted("ROLE_FOSUSER_NOUVEAU")
      */
-    public function create(Request $request): FosUser {
+    public function create(Request $request): FosUser
+    {
         $fosUser = new FosUser();
         $form = $this->createForm(FosUserType::class, $fosUser);
         $form->submit(Utils::serializeRequestContent($request));
@@ -167,7 +198,8 @@ class FosUserController extends AbstractController {
      * @Rest\View(StatusCode=200)
      * @IsGranted("ROLE_FOSUSER_AFFICHAGE")
      */
-    public function show(FosUser $fosUser): FosUser {
+    public function show(FosUser $fosUser): FosUser
+    {
         return $fosUser;
     }
 
@@ -176,7 +208,8 @@ class FosUserController extends AbstractController {
      * @Rest\View(StatusCode=200)
      * @IsGranted("ROLE_FOSUSER_EDITION")
      */
-    public function edit(Request $request, FosUser $fosUser): FosUser {
+    public function edit(Request $request, FosUser $fosUser): FosUser
+    {
         $form = $this->createForm(FosUserType::class, $fosUser);
         $form->submit(Utils::serializeRequestContent($request));
 
@@ -190,7 +223,8 @@ class FosUserController extends AbstractController {
      * @Rest\View(StatusCode=200)
      * @IsGranted("ROLE_FOSUSER_CLONE")
      */
-    public function cloner(Request $request, FosUser $fosUser): FosUser {
+    public function cloner(Request $request, FosUser $fosUser): FosUser
+    {
         $em = $this->getDoctrine()->getManager();
         $fosUserNew = new FosUser();
         $form = $this->createForm(FosUserType::class, $fosUserNew);
@@ -207,7 +241,8 @@ class FosUserController extends AbstractController {
      * @Rest\View(StatusCode=200)
      * @IsGranted("ROLE_FOSUSER_DELETE")
      */
-    public function delete(FosUser $fosUser): FosUser {
+    public function delete(FosUser $fosUser): FosUser
+    {
         $entityManager = $this->getDoctrine()->getManager();
         $entityManager->remove($fosUser);
         $entityManager->flush();
@@ -220,7 +255,8 @@ class FosUserController extends AbstractController {
      * @Rest\View(StatusCode=200)
      * @IsGranted("ROLE_FOSUSER_DELETE")
      */
-    public function deleteMultiple(Request $request): array {
+    public function deleteMultiple(Request $request): array
+    {
         $entityManager = $this->getDoctrine()->getManager();
         $fosUsers = Utils::getObjectFromRequest($request);
         if (!count($fosUsers)) {
