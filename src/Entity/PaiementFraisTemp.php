0644c3d3e30d51fa2ef44c0746a66277a062b93c
Mouhmadoul Amine THIAM <mamine.thiam@univ-thies.sn>
2021-11-25 10:01:53 +0000

    https://uidt.atlassian.net/browse/GPE-65

---
+++ src/Entity/PaiementFraisTemp.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * PaiementFraisTemp
+ *
+ * @ORM\Table(name="paiement_frais_temp")
+ * @ORM\Entity
+ */
+class PaiementFraisTemp
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="montant", type="decimal", precision=10, scale=0, nullable=false)
+     */
+    private $montant;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="ref_command", type="string", length=150, nullable=false)
+     */
+    private $refCommand;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date", type="datetime", nullable=false, options={"default"="CURRENT_TIMESTAMP"})
+     */
+    private $date = 'CURRENT_TIMESTAMP';
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="inscriptionacad", type="integer", nullable=false)
+     */
+    private $inscriptionacad;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getMontant(): ?string
+    {
+        return $this->montant;
+    }
+
+    public function setMontant(string $montant): self
+    {
+        $this->montant = $montant;
+
+        return $this;
+    }
+
+    public function getRefCommand(): ?string
+    {
+        return $this->refCommand;
+    }
+
+    public function setRefCommand(string $refCommand): self
+    {
+        $this->refCommand = $refCommand;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getInscriptionacad(): ?int
+    {
+        return $this->inscriptionacad;
+    }
+
+    public function setInscriptionacad(int $inscriptionacad): self
+    {
+        $this->inscriptionacad = $inscriptionacad;
+
+        return $this;
+    }
+
+
+}
